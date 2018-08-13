1c9c9dde824f563609475c113a026a38f2ee1660
antiproton85 <kevinmcgr@gmail.com>
2018-08-13 14:29:43 -0400

    Updated the thankyou page

---
+++ components/SymphonyLinks.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="sym-links">
+        <a href="#">See how our customers are using Symphony to improve their processes</a>
+        <a href="#">傍め果結リユ別16追レけほ況有だば稿関イメタシ書負ら志我30預っう券新ミ民目ツウ交早目っねき目容ッださ次面問まだ共当るぴえ紀人わの属配幌く。</a>
+        <a href="#">לשון פיסיקה מדריכים בה שתי. מה ויש הרוח ובמתן ויקיפדיה, מה הקהילה ביולוגיה ויקיפדיה אחר, סדר שתפו בדפים אגרונומיה בה. חפש ריקוד עקרונות את.</a>
+    </div>
+</template>
+
+<script>
+export default {
+    //http://generator.lorem-ipsum.info/_hebrew
+}
+</script>
+
+
+<style scoped>
+    .sym-links a {
+        display: inline-block;
+        margin-bottom: 10px;
+    }
+
+    .sym-links a:link {
+        color: #0395a1;
+    }
+</style>
