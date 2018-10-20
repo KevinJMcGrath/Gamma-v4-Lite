82aa2aa207006acca42049e14c1b03c92d64bf29
antiproton85 <kevinmcgr@gmail.com>
2018-10-20 09:47:47 -0400

    Match header text justification with page content.

--- components/SymphonyHeader.vue
+++ components/SymphonyHeader.vue
@@ -4,7 +4,11 @@
             <div class="lite-header-logo"></div>
         </div>
         <div class="lite-banner">
-            <p>Sign up</p>
+            <Row>
+                <i-col span=10 offset=4 class="lite-col">
+                    <p>Sign up</p>
+                </i-col>
+            </Row>
         </div>
     </div>
 </template>
