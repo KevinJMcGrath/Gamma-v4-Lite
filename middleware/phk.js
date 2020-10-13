3292456d19e438463b5afb41dce6e9752def15a0
antiproton85 <kevinmcgr@gmail.com>
2020-10-13 05:13:40 -0400

    *Remove* isDev check in phk.js
    Add comment to remind myself how I am doing this now

--- middleware/phk.js
+++ middleware/phk.js
@@ -1,26 +1,24 @@
-export default async function ({route, store, query, redirect, isDev}) {
-    if (!isDev) {
-        let phk_token = query != null && query.phk != null ? query.phk : ''        
+export default async function ({route, store, query, redirect}) {
 
-        if (phk_token) {
-            store.commit('SET_PHK', phk_token)
-        }
-        
-        if (!route.name || route.name === 'unknown' || route.name === 'index') {
-            console.log('Skipping PHK redirect by route name...')
-        } else {
-            
-            let phk_verified = await store.dispatch('verifyPHK', phk_token)
+    // Use the USE_PHK env variable to disable this functionality
+    
+    let phk_token = query != null && query.phk != null ? query.phk : ''        
 
-            if (!phk_verified) {
-                console.log('PHK invalid')
-                redirect('https://www.symphony.com')
-            }
-        }
+    if (phk_token) {
+        store.commit('SET_PHK', phk_token)
     }
-
+    
+    if (!route.name || route.name === 'unknown' || route.name === 'index') {
+        console.log('Skipping PHK redirect by route name...')
+    } else {
         
-   
+        let phk_verified = await store.dispatch('verifyPHK', phk_token)
+
+        if (!phk_verified) {
+            console.log('PHK invalid')
+            redirect('https://www.symphony.com')
+        }
+    }   
 }
 
     
