c9bc5fba3a77a7c0204655caf35ba69bc3815d66
antiproton85 <kevinmcgr@gmail.com>
2020-10-13 05:05:59 -0400

    Exclude phk check if dev

--- middleware/phk.js
+++ middleware/phk.js
@@ -1,23 +1,25 @@
 export default async function ({route, store, query, redirect, isDev}) {
-    let phk_token = query != null && query.phk != null ? query.phk : ''
+    if (!isDev) {
+        let phk_token = query != null && query.phk != null ? query.phk : ''        
 
-    console.log('PHK MW Check QP: ' + phk_token)
-
-    if (phk_token) {
-        store.commit('SET_PHK', phk_token)
-    }
-    
-    if (!route.name || route.name === 'unknown' || route.name === 'index') {
-        console.log('Skipping PHK redirect by route name...')
-    } else {
-        
-        let phk_verified = await store.dispatch('verifyPHK', phk_token)
-
-        if (!phk_verified) {
-            console.log('PHK invalid')
-            redirect('https://www.symphony.com')
+        if (phk_token) {
+            store.commit('SET_PHK', phk_token)
         }
-    }    
+        
+        if (!route.name || route.name === 'unknown' || route.name === 'index') {
+            console.log('Skipping PHK redirect by route name...')
+        } else {
+            
+            let phk_verified = await store.dispatch('verifyPHK', phk_token)
+
+            if (!phk_verified) {
+                console.log('PHK invalid')
+                redirect('https://www.symphony.com')
+            }
+        }
+    }
+
+        
    
 }
 
